abf121903e4cd9c18fbce537bea9540e7fcf43c0
Evan Ryan <Evan Ryan>
2017-03-06 12:52:26 -0500

    current

---
+++ k.js
@@ -0,0 +1,10 @@
+var buttonElement = document.getElementById('theme')
+
+buttonElement.addEventListener('click', function () {
+  document.body.setAttribute('theme', 'day')
+}
+)
+var theme = window.localstorage.getItem('theme', theme)
+var theme2 = window.localStorage.setItem('theme',)
+
+var visit = window.localStorage.getItem('visits')
